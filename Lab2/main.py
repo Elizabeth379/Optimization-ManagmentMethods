bf24b136b164c3606e4276fb916e0e96f153c1e1
Elizabeth <bellator.379@gmail.com>
2024-02-16 18:04:51 +0300

    Refactoring

--- Lab2/main.py
+++ Lab2/main.py
@@ -10,27 +10,27 @@ def simplex_method(c, x, A, b, B):
 
     # Основной цикл симплекс-метода
     while True:
-        #Шаг 1
+        #Шаг 1 - строим базисную матрицу и находим обратную
         B_matrix = A[:, B]
         B_inv = np.linalg.inv(B_matrix)
 
-        #Шаг 2
+        #Шаг 2 - вектор компонент вектора С
         c_B = c[B]
 
-        # Шаг 3
+        # Шаг 3 - вектор потенциалов
         u = c_B @ B_inv
-        # Шаг 4
+        # Шаг 4 - вектор оценок
         delta_c = u @ A - c
 
-        # Проверка на оптимальность
+        # Шаг 5 - Проверка на оптимальность
         if np.all(delta_c >= 0):
             assert np.all(A @ x == b)
             return x_new
 
-        # Шаг 6
+        # Шаг 6 - находим первую отрицат компоненту
         j0 = np.where(delta_c < 0)[0][0]
 
-        # Шаг 7
+        # Шаг 7 - базисную обратную умножаем на столбец матрицы А с индексом j0
         z = B_inv @ A[:, j0]
 
         # Шаг 8
@@ -43,14 +43,14 @@ def simplex_method(c, x, A, b, B):
         if theta0 == np.inf:
             raise Exception("Целевая функция не ограничена сверху на множестве допустимых планов")
 
-        # Step 11
+        # Шаг 11
         k = np.argmin(theta)
         j_star = B[k]
 
-        # Step 12
+        # Шаг 12
         B[k] = j0
 
-        # Step 12
+        # Шаг 13
         for i in range(m):
             if i != k:
                 x_new[B[i]] -= theta0 * z[i]
